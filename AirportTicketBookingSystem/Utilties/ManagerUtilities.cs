6eb14d21858d66244b9ee802e5632e93efe2220f
Amro <amroqadadha@gmail.com>
2024-08-28 21:28:50 +0300

    Add ManagerUtilities.cs with Filter Booking feature

---
+++ AirportTicketBookingSystem/Utilties/ManagerUtilities.cs
@@ -0,0 +1,91 @@
+﻿using AirportTicketBookingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportTicketBookingSystem.Utilties
+{
+    public class ManagerUtilities
+    {
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Welcome Manager!");
+            Console.WriteLine("First Name");
+            string? firstName = Console.ReadLine();
+            Console.WriteLine("Last Name");
+            string? lastName = Console.ReadLine();
+            Manager manager = new Manager(firstName, lastName);
+            while (true)
+            {
+                Console.WriteLine("1-Filter Bookings\n0-Go back");
+                int op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 0: return;
+                    case 1:
+                        FilterBookings();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            }
+        }
+        public static void FilterBookings()
+        {
+            Console.WriteLine("Filter by\n1- Booking ID\n2- Flight ID\n3- Passenger ID\n4- Flight Information");
+            int op = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("Enter Booking ID");
+                    int bookingId = Convert.ToInt32(Console.ReadLine());
+                    Booking? booking = Manager.AllBookings!.FirstOrDefault(b => b.BookingId == bookingId);
+                    if (booking == null)
+                    {
+                        Console.WriteLine("Booking not found");
+                    }
+                    else
+                    {
+                        Console.WriteLine(booking);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Enter flight id");
+                    int flightId = Convert.ToInt32(Console.ReadLine());
+                    List<Booking>? bookings = Manager.AllBookings!.Where(b => b.Flight.FlightId == flightId).ToList();
+                    if (bookings.Count == 0)
+                    {
+                        Console.WriteLine("No bookings found");
+                    }
+                    else
+                    {
+                        foreach (var book in bookings)
+                        {
+                            Console.WriteLine(book);
+                        }
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Enter passenger id");
+                    int passengerId = Convert.ToInt32(Console.ReadLine());
+                    List<Booking>? bookingsByPassenger = Manager.AllBookings!.Where(b => b.PassengerId == passengerId).ToList();
+                    if (bookingsByPassenger.Count == 0)
+                    {
+                        Console.WriteLine("No bookings found");
+                    }
+                    else
+                    {
+                        foreach (var book in bookingsByPassenger)
+                        {
+                            Console.WriteLine(book);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
