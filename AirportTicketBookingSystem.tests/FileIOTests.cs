bdf4136f2636c30809fd6f526319a96b526348c2
Amro <amroqadadha@gmail.com>
2024-09-18 20:21:20 +0300

    Add unit tests for flight validation

--- AirportTicketBookingSystem.tests/FileIOTests.cs
+++ AirportTicketBookingSystem.tests/FileIOTests.cs
@@ -3,6 +3,7 @@ using AirportTicketBookingSystem.Models;
 using AirportTicketBookingSystem.Utilties;
 using AirportTicketBookingSystem.Utilties.ManagerOptionsHandling.ErrorHandling;
 using AutoFixture;
+using AutoFixture.AutoMoq;
 using Moq;
 
 namespace AirportTicketBookingSystem.tests
@@ -11,26 +12,52 @@ namespace AirportTicketBookingSystem.tests
     {
         public FileIOTests()
         {
-            GeneralUtility.flights.Clear();
         }
         [Fact]
-        public async void ShouldValidateFlights()
+        public void ShouldValidateFlight()
         {
-            var mockFlightValidator = new Mock<IFlightValidator>();
-            FlightImport flightImport = new FlightImport(mockFlightValidator.Object);
-            await flightImport.ImportFromCsvAsync();
-            mockFlightValidator.Verify(x => x.ValidateFlight(It.IsAny<IFlight>()), Times.Exactly(20));
-            mockFlightValidator.Verify(x => x.PrintValidationResults(), Times.Once);
+            Mock<IErrorLogger> logger = new Mock<IErrorLogger>();
+            logger.Setup(x => x.ErrorMessages).Returns(new List<string>());
+            FlightValidator flightValidator = new FlightValidator(logger.Object);
+            IFixture fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var flight = fixture.Build<Flight>()
+                                .With(f => f.FlightId, 100)
+                                .With(f => f.DepartureDate, new DateTime(2025, 9, 5))
+                                .Create();
+            flightValidator.ValidateFlight(flight);
+            Assert.Contains(GeneralUtility.flights, fligt => flight.FlightId == 100);
+            GeneralUtility.flights.Remove(flight);
         }
-        [Fact(Skip ="Issue with async await")]
-        public async void ShouldImportAllValidFlights()
+        [Fact]
+        public void ShouldNotValidateFlight()
+        {
+            Mock<IErrorLogger> logger = new Mock<IErrorLogger>();
+            logger.Setup(x => x.ErrorMessages).Returns(new List<string>());
+            FlightValidator flightValidator = new FlightValidator(logger.Object);
+            IFixture fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var flight = fixture.Build<Flight>()
+                                .With(f => f.FlightId, 100)
+                                .With(f => f.DepartureDate, new DateTime(2025, 9, 5))
+                                .Create();
+            GeneralUtility.flights.Add(flight);
+            int count = GeneralUtility.flights.Count;
+            flightValidator.ValidateFlight(flight);
+            Assert.Equal(count, GeneralUtility.flights.Count);
+            Assert.Contains(logger.Object.ErrorMessages, message => message.Contains($"Issue with flight 100: Flight with ID {flight.FlightId} already exists."));
+            GeneralUtility.flights.Remove(flight);
+
+        }
+        [Fact]
+        public async Task ShouldImportAllValidFlights()
         {
             var mockErrorLogger = new Mock<IErrorLogger>();
             mockErrorLogger.Setup(x => x.ErrorMessages).Returns(new List<string>());
-            FlightValidator flightValidator = new FlightValidator(mockErrorLogger.Object);
-            FlightImport flightImport = new FlightImport(flightValidator);
+
+            var mockFlightValidator = new Mock<IFlightValidator>();
+            var flightImport = new FlightImport(new FlightValidator(mockErrorLogger.Object));
 
             await flightImport.ImportFromCsvAsync();
+
             Assert.Equal(18, GeneralUtility.flights.Count);
             Assert.Equal(2, mockErrorLogger.Object.ErrorMessages.Count);
         }
